25582401435d33b10812157ed572bdd7d575d247
Steven Schubiger (stsc) <schubiger@cpan.org>
2004-01-06 16:37:28 +0000

    Import of SCHUBIGER/Math-Factor-0.03 from CPAN.
    gitpan-cpan-distribution: Math-Factor
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         SCHUBIGER/Math-Factor-0.03.tar.gz
    gitpan-cpan-author:       SCHUBIGER
    gitpan-cpan-maturity:     released

--- bin/factor.pl
+++ bin/factor.pl
@@ -5,12 +5,10 @@ use warnings;
 
 my (@numbers, %factors, %match);
 
-@numbers = qw(9 50 102 2321 30107
-);
+@numbers = qw(9 50 102 2321 30107);
 
 %factors = factor(\@numbers);
 show_factor(\%factors);
 
 %match = match(\%factors);
 show_match(\%match);
-
